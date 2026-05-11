ff7998595fe8b2f91faf8de831ef06ce383f1aa8
NeuraiProject <code@neurai.org>
2026-05-11 11:25:16 +0200

    feat: Add signing configuration for release builds and implement a debug keystore

--- components/WalletsCarousel.tsx
+++ components/WalletsCarousel.tsx
@@ -57,7 +57,6 @@ const nStyles = StyleSheet.create({
   },
   button: {
     marginTop: 12,
-    backgroundColor: '#007AFF',
     paddingHorizontal: 32,
     paddingVertical: 12,
     borderRadius: 8,
@@ -121,8 +120,8 @@ const NewWalletPanel: React.FC<NewWalletPanelProps> = ({ onPress }) => {
       >
         <Text style={[nStyles.addAWAllet, { color: colors.foregroundColor }]}>{loc.wallets.list_create_a_wallet}</Text>
         <Text style={[nStyles.addLine, { color: colors.alternativeTextColor }]}>{loc.wallets.list_create_a_wallet_text}</Text>
-        <View style={nStyles.button}>
-          <Text style={[nStyles.buttonText, { color: colors.brandingColor }]}>{loc.wallets.list_create_a_button}</Text>
+        <View style={[nStyles.button, { backgroundColor: colors.newBlue }]}>
+          <Text style={[nStyles.buttonText, { color: colors.buttonTextColor }]}>{loc.wallets.list_create_a_button}</Text>
         </View>
       </Animated.View>
     </Pressable>
